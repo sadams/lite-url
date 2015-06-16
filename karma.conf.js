b5348fbf92077bcd8bb503bdf387a3d08868cf78
Sam Adams <adams.sam@gmail.com>
2015-06-16 21:25:58 +0100

    fixing syntax error

--- karma.conf.js
+++ karma.conf.js
@@ -63,7 +63,7 @@ module.exports = function(config) {
     });
     if (process.env.TRAVIS) {
         console.log('env: ',
-            JSON.stringify(process.env);
+            JSON.stringify(process.env)
         );
         config.sauceLabs.build = 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')';
         config.sauceLabs.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
